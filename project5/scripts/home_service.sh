09d65e5efb267967a54dcff4a0b779d13a1db328
luckymeng7 <luckymeng7@gmail.com>
2023-03-02 05:06:27 +0000

    add home_service script

---
+++ project5/scripts/home_service.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+xterm  -e  " roslaunch my_robot world.launch " &
+sleep 5
+xterm  -e  " roslaunch my_robot amcl.launch " &
+sleep 5
+xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
+sleep 5
+xterm  -e  " rosrun pick_objects pick_objects " &
+sleep 5
+xterm  -e  " rosrun add_markers add_markers "
