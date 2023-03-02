a8732d6aafac6943fe061b7dddd17643ef848641
luckymeng7 <luckymeng7@gmail.com>
2023-03-02 04:33:53 +0000

    update the shell script and cpp file for add_markers

--- project5/add_markers/src/add_markers.cpp
+++ project5/add_markers/src/add_markers.cpp
@@ -6,13 +6,14 @@ int main( int argc, char** argv )
   ros::init(argc, argv, "add_markers");
   ros::NodeHandle n;
   ros::Rate r(1);
-  ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
+  ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1, true);
 
   // Set our initial shape type to be a cube
   uint32_t shape = visualization_msgs::Marker::CUBE;
 
-  while (ros::ok())
-  {
+ 
+////// Publish the 1st marker at pickup zone ///////////
+
     visualization_msgs::Marker marker;
     // Set the frame ID and timestamp.  See the TF tutorials for information on these.
     marker.header.frame_id = "map";
@@ -20,7 +21,7 @@ int main( int argc, char** argv )
 
     // Set the namespace and id for this marker.  This serves to create a unique ID
     // Any marker sent with the same namespace and id will overwrite the old one
-    marker.ns = "add_markers";
+    marker.ns = "pickup_objects";
     marker.id = 0;
 
     // Set the marker type.  Initially this is CUBE, and cycles between that and SPHERE, ARROW, and CYLINDER
@@ -39,9 +40,9 @@ int main( int argc, char** argv )
     marker.pose.orientation.w = 1.0;
 
     // Set the scale of the marker -- 1x1x1 here means 1m on a side
-    marker.scale.x = 1.0;
-    marker.scale.y = 1.0;
-    marker.scale.z = 1.0;
+    marker.scale.x = 0.2;
+    marker.scale.y = 0.2;
+    marker.scale.z = 0.2;
 
     // Set the color -- be sure to set alpha to something non-zero!
     marker.color.r = 0.0f;
@@ -49,37 +50,25 @@ int main( int argc, char** argv )
     marker.color.b = 0.0f;
     marker.color.a = 1.0;
 
-    marker.lifetime = ros::Duration();
-
-    // Publish the marker
-    while (marker_pub.getNumSubscribers() < 1)
-    {
-      if (!ros::ok())
-      {
-        return 0;
-      }
-      ROS_WARN_ONCE("Please create a subscriber to the marker");
-      sleep(1);
-    }
+    marker.lifetime = ros::Duration();    
+    
+    ROS_INFO("Marker appears at the pickup zone");
     marker_pub.publish(marker);
+    ros::Duration(5.0).sleep();
 
-    // Cycle between different shapes
-    switch (shape)
-    {
-    case visualization_msgs::Marker::CUBE:
-      shape = visualization_msgs::Marker::SPHERE;
-      break;
-    case visualization_msgs::Marker::SPHERE:
-      shape = visualization_msgs::Marker::ARROW;
-      break;
-    case visualization_msgs::Marker::ARROW:
-      shape = visualization_msgs::Marker::CYLINDER;
-      break;
-    case visualization_msgs::Marker::CYLINDER:
-      shape = visualization_msgs::Marker::CUBE;
-      break;
-    }
+    marker.action = visualization_msgs::Marker::DELETE;
+    marker_pub.publish(marker);
+    ROS_INFO("Marker is hidden");
+    ros::Duration(5.0).sleep();
 
-    r.sleep();
-  }
+    // Set new pose of the marker.
+    marker.pose.position.x = -2;
+    marker.pose.position.y = 2.5;
+    marker.action = visualization_msgs::Marker::ADD;
+    marker_pub.publish(marker);
+    ROS_INFO("Marker appears at the drop off zone");
+    ros::Duration(20.0).sleep();
+
+    
+ 
 }
